18178323f908c44c4815b0aaeec8ba5dc4a700b7
raquis <rizalpratama10010@gmail.com>
2025-01-23 09:09:16 +0700

    feat: add list pokemon

---
+++ html.js
@@ -0,0 +1,11 @@
+const eLoading = (title = "Loading...", customClass = "") => `<div class="has-text-centered py-6 ${customClass}">${title}</div>`;
+
+const eCard = (title, callback = () => {}) => `
+  <div class="card">
+    <div class="card-content">
+      <div class="content">
+        <div class="has-text-primary" onclick="${callback()}" style="cursor: pointer;">${title}</a>
+      </div>
+    </div>
+  </div>
+`;
